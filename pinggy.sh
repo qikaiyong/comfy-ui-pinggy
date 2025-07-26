9b51535d7b44853a2601e1c3ade3dccd55a98249
东华 <30590255+qikaiyong@users.noreply.github.com>
2025-07-26 16:10:01 +0800

    Update pinggy.sh

--- pinggy.sh
+++ pinggy.sh
@@ -1,1 +1,1 @@
-./pinggy -p 443 -R0:localhost:8188 -L4300:localhost:4300 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 PQiHccJpcr4@free.pinggy.io
+./pinggy -p 443 -R0:localhost:8188 -L4300:localhost:4300 -o StrictHostKeyChecking=no -o ServerAliveInterval=30 PQiHccJpcr4@free.pinggy.io --log log.txt
